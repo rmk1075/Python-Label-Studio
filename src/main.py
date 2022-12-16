5d506fcceb2fdc8a93ffad7904394ea3a04c10a2
rmk1075 <rmk1075@gmail.com>
2022-12-16 17:37:22 +0900

    feat: add initialize() - load environment variable from .env file and init constant vars

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
-# Import the SDK and the client module
+from dotenv import load_dotenv
 from label_studio_sdk import Client
+import os
 from typing import List
 
 # Define the URL where Label Studio is accessible and the API key for your user account
@@ -7,6 +8,16 @@ LABEL_STUDIO_URL = ''
 API_KEY = ''
 
 IMAGE_FILE = './resource/images/test.jpg'
+ENV_FILE = './resource/.env'
+
+def initialize() -> None:
+    load_dotenv(dotenv_path=ENV_FILE)
+
+    global LABEL_STUDIO_URL
+    LABEL_STUDIO_URL = os.environ.get('LABEL_STUDIO_URL')
+
+    global API_KEY
+    API_KEY = os.environ.get('API_KEY')
 
 
 '''
@@ -16,6 +27,8 @@ IMAGE_FILE = './resource/images/test.jpg'
 - pull labels (label studio image - autocare_tx imagedata mapping logic)
 '''
 if __name__ == "__main__":
+    initialize()
+
     # Connect to the Label Studio API and check the connection
     client = Client(url=LABEL_STUDIO_URL, api_key=API_KEY)
     print(client.check_connection())
